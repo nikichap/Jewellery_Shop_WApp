d6178e455fe320981e4621f7798a28465aec29f4
nikichap <niki.chapanova@gmail.com>
2022-03-23 20:54:43 +0200

    View doesn't work

---
+++ Models/DTOs/FavouritesDTO.cs
@@ -0,0 +1,16 @@
+﻿using Jewellery_Shop.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jewellery_Shop.Models.DTOs
+{
+    public class FavouritesDTO
+    {
+        public int Id { get; set; }
+        public Item Item { get; set; }
+        public string CreatedBy { get; set; }
+
+    }
+}
